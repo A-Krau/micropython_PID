f3e54c86b1c4c330304bbd7150aa990a53b0a5e7
Aric <aric.j.krause@gmail.com>
2021-04-10 15:43:28 -0700

    Update PID.py

--- PID.py
+++ PID.py
@@ -8,13 +8,21 @@ https://www.teachmemicro.com/arduino-pid-control-tutorial/
 class pid():
     """My attempt at writing a PID class"""
     def __init__(self):
+
         self.previous_time = 0
         self.set_point = 0
         self.cumu_error = 0
         self.last_error = 0
 
+        self.kp = 0.0003
+        self.ki = 0
+        self.kd = 0.005
+        
+        #print in the terminal to make sure my upload actually took and the values were updated.
+        print(self.kp, self.ki, self.kd)
+
     """To tune a PID use the following steps:
-    1. Set all gains to zero.
+    1.Set all gains to zero.
     2. Increase the P gain until the response to a disturbance is steady oscillation.
     3. Increase the D gain until the the oscillations go away (i.e. it's critically damped).
     4. Repeat steps 2 and 3 until increasing the D gain does not stop the oscillations.
@@ -26,12 +34,7 @@ class pid():
     This worked best for me and was found here...
     https://robotics.stackexchange.com/questions/167/what-are-good-strategies-for-tuning-pid-loops
     """        
-        self.kp = 0.0003
-        self.ki = 0
-        self.kd = 0.005
-        
-        #print in the terminal to make sure my upload actually took and the values were updated.
-        print(self.kp, self.ki, self.kd)
+
 
     def comp_pid(self, sensor_pos, set_point):
         """Compute PID value based on a sensors postion and a given set point.  
@@ -45,7 +48,7 @@ class pid():
         self.cumu_error += self.error * self.elapsed_time
         try:
             self.rate_error = (self.error - self.last_error)/self.elapsed_time
-        #except the zero error I was getting at times. Putting that specific error in the except line didnt work, so lets just except them all, shall we?
+        #except the zero error I was getting at times
         except:
             pass
             
